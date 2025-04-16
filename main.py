1e8a262b6cb50f65e35e4335ef55c59dd0f0a291
noheartpen <3104408599@qq.com>
2025-04-16 21:51:01 +0900

    chore: black

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from mikann import analyze_text,_cursor_word
+
+from mikann import analyze_text, _cursor_word
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class SentenceRequest(BaseModel):
@@ -9,29 +19,46 @@ class SentenceRequest(BaseModel):
     cursor_index: str
 
 
+class AnalyzeRequest(BaseModel):
+    sentence: str
+    cursor_index: int
+
+
 @app.post("/")
-async def root(request: SentenceRequest):
+def post_analyze(request: AnalyzeRequest):
     try:
+        sentence = request.sentence
+        cursor_index = request.cursor_index
         # 处理输入的句子
-        print(request.sentence)
-        return {"message": "Hello World"}
+        print(f"Sentence: {sentence}, Cursor Index: {cursor_index}")
+        result = analyze_text(sentence)
+        cursor_jishokei = _cursor_word(result, cursor_index)
+        print(cursor_jishokei)
+        return {"jishokei": f"{cursor_jishokei}"}
+
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
 @app.get("/")
-async def analyze(sentence: str = Query(..., description="Input sentence"),
-                  cursor_index: int = Query(..., description="Cursor index")):
+def get_analyze(
+    sentence: str = Query(..., description="Input sentence"),
+    cursor_index: int = Query(..., description="Cursor index"),
+):
+    """
+
+    Args:
+        sentence: 完整的上下文。
+        cursor_index: 光标在句子中的位置。
+
+    Returns:
+
+    """
     try:
-        # 处理输入的句子
         print(f"Sentence: {sentence}, Cursor Index: {cursor_index}")
         result = analyze_text(sentence)
-        return _cursor_word(result, cursor_index)
-        # return {"message": "Processing successful", "sentence": sentence, "cursor_index": cursor_index}
+        cursor_jishokei = _cursor_word(result, cursor_index)
+        print(cursor_jishokei)
+        return {"jishokei": f"{cursor_jishokei}"}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
--- mikann.py
+++ mikann.py
@@ -4,18 +4,17 @@ from sudachipy import Dictionary
 def analyze_text(text) -> list:
     tokenizer = Dictionary().create()
     result_list = []
+    # TODO: 移除常见的 markdown 语法和 ruby 标签
     for token in tokenizer.tokenize(text):
-        result = []
-        result.append(token.surface())
-        result.append(token.dictionary_form())
+        result = [token.surface(), token.dictionary_form()]
+        # TODO 注意检查 SudachiDict
         # result.append(token.reading_form)
         result_list.append(result)
     return result_list
 
 
-def _cursor_word(analysis_result: list, cursor_index: int) -> str:
-    """获取光标附近
-    """
+def _cursor_word(analysis_result: list[str], cursor_index: int) -> str | None:
+    """获取光标附近"""
     # if sum(len(key) for key in result) < cursor_index:
     #     # 如果光标所在的位置大于文本的长度，直接抛出异常
     #     raise ValueError("Cursor index is out of range.")
@@ -26,7 +25,6 @@ def _cursor_word(analysis_result: list, cursor_index: int) -> str:
         length_before_cursor += len(result[0])
         if length_before_cursor >= cursor_index:
             # TODO = 说明用户的光标正好放在2个句节的分界处
-            # 可以考虑基于难度和词频猜测哪个更有可能是用户想查的单词，
+            # TODO 可以考虑基于难度和词频猜测哪个更有可能是用户想查的单词，
             # 或者允许按照个人习惯，设置这种情况是返回前还是后
             return result[1]
-
