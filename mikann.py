e2c82be033ea5f0930fe407472286c4099cc19f0
John Qing <3104408599@qq.com>
2025-04-17 19:48:37 +0900

    feat: jishokei

--- mikann.py
+++ mikann.py
@@ -1,14 +1,40 @@
 from sudachipy import Dictionary
 
+def has_not_kana(input_text: str) -> bool:
+    """Check if the input text does not have kana.
+
+    Args:
+        input_text: A string to check.
+
+    Returns:
+        bool: True if no kana is found, False otherwise.
+    """
+    for char in input_text:
+        gana_code = ord(char)
+        if 12353 <= gana_code <= 12447 or 12448 <= gana_code <= 12543:
+            # According to the Unicode Office Document, the Hiragana code ranges from 12353 to 12447.
+            # https://www.unicode.org/charts/PDF/U3040.pdf 
+            # And the Katakana code ranges from 12448 to 12543.
+            # https://www.unicode.org/charts/PDF/U30A0.pdf
+            return False
+    # If no kana characters are found, return True
+    return True
+
 
 def analyze_text(text) -> list:
     tokenizer = Dictionary().create()
     result_list = []
     # TODO: 移除常见的 markdown 语法和 ruby 标签
     for token in tokenizer.tokenize(text):
-        result = [token.surface(), token.dictionary_form()]
-        # TODO 注意检查 SudachiDict
-        # result.append(token.reading_form)
+        # 辞書の見出しの漢字表記と Sudachi の正規化表記と違うところが多いので
+        # 普段、読み方（reading_form）で検索するのをお勧め。
+        jishokei = token.reading_form()
+        if has_not_kana(token.dictionary_form):
+            # dictionary_form に仮名が含まれていない場合、同音異義語の可能性が高いため、読み方で検索すると結果が煩雑になることがある。そのため、dictionary_formで検索するのをお勧め。
+            # 日本語において最も同音異義語が多いとされる熟語は「こうしょう」であり、『スーパー大辞林3.0』では  語が該当する（交渉・考証・工匠・高尚・鉱床・口承・厚相・哄笑・公称・工廠・公証・公娼・校章など）。『広辞苑』第 6 版には 50 もの仮名見出しがある。
+            # https://ja.wikipedia.org/wiki/%E5%90%8C%E9%9F%B3%E7%95%B0%E7%BE%A9%E8%AA%9E
+            jishokei = token.dictionary_form
+        result = [token.surface(), jishokei]
         result_list.append(result)
     return result_list
 
